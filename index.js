ecd8e106171171d3ea5e6d0a01b12a1edd714a4f
Conor Murnane <conormurnane11@gmail.com>
2021-11-06 15:10:29 -0400

    back to basics cont

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-var five = 5; //number 5
+var a= 5;
+a = 8;
+console.log(a)
 
+var b;
+b = 5;
+a = a + b;
+var c = "I am a ";
+c = c + "String";
+console.log(a,c);
+d = 'hi';
+console.log(d)
 /*
 Data types:
 undefined, null, boolean, string, symbol, number, object
