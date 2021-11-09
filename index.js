e3293d3b92c5365d8364d290f9696a5cb524ff3d
Conor Murnane <conormurnane11@gmail.com>
2021-11-08 20:23:03 -0500

    array review ...

--- index.js
+++ index.js
@@ -50,4 +50,45 @@ CODE    OUTPUT
 
 
 var myStr = "I am a \n\"double quoted\" string inside \t\"double quotes\" "
-console.log(myStr)
+// console.log(myStr)
+
+var str = "Jello World"
+
+str[0] = "H"
+// console.log(str)
+
+var arr = ["J","E","L"]
+// console.log(arr)
+arr[0] = "H"
+// console.log(arr)
+
+
+function wordBlanks(myNoun,myAdjective,myVerb,myAdverb){
+    var result = ""
+    result += "The " + " " + myAdjective + " " + myNoun + " " + myVerb + " to the store " + myAdverb + "."
+    return result
+}
+
+console.log(wordBlanks("dog","big","ran","quickly"))
+console.log(wordBlanks("bike","slow","flew","slowly"))
+
+//                   ******************* Arrays ********************
+
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12]], [23,14]]
+
+myArray[3] = [10,00,1]
+console.log(myArray)
+console.log(myArray[2][1])
+myArray.push(['hot soup',2])
+console.log(myArray)
+
+myArray.pop()
+console.log(myArray)
+myArray.shift()
+console.log(myArray)
+
+// push add to end o(1)
+// unshift, add to front o(N)
+
+// pop remove from end o(1)
+// shift remove from front  o(N)
